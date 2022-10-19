752a0eb4a4e754e4cdb1a68c16d243319e9bef30
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-19 14:45:29 +0200

    removed buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,30 @@
-/* 
-Visualizzare in pagina 5 numeri casuali.
-Da lì parte un timer di 30 secondi. 
-Dopo 30 secondi i numeri scompaiono
-l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt(). 
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-*/
-
 // seleziono l'elemento della DOM dove mostrare i numeri
 const containerEl = document.querySelector('.container');
 
-// seleziono i pulsanti 
-const startEl = document.querySelector('.start');
-const stopEl = document.querySelector('.stop');
+// creo una variabile per la scelta dell'utente
+let userChoice = [];
 
 // creo una variabile per i secondi
 let seconds = 30 * 1000;
 
-// aggiungo un eventlistener per i pulsanti
-startEl.addEventListener('click', function() {
-    // genero 5 numeri casuali con un ciclo for
-    for (let i = 0; i < 5; i++) {
-        let numGen = getRndInteger(1, 100);
-    
-        // creo un elemento nella DOM dove salvare i numeri generati
-        const numbers = document.createElement('div');
-        numbers.classList.add('numbers');
-        numbers.innerHTML = `<div class="numbers">${numGen}</div>`;
-    
-        // inserisco l'elemento appena creato nella DOM
-        containerEl.appendChild(numbers);
+// genero 5 numeri casuali con un ciclo for
+for (let i = 0; i < 5; i++) {
+    let numGen = getRndInteger(1, 100);
 
-        setTimeout(() => {
-            numbers.style.display = 'none';
-        }, seconds)
-    }
-})
+    // creo un elemento nella DOM dove salvare i numeri generati
+    const numbers = document.createElement('div');
+    numbers.classList.add('numbers');
+    numbers.innerHTML = `<div class="numbers">${numGen}</div>`;
+
+    // inserisco l'elemento appena creato nella DOM
+    containerEl.appendChild(numbers);
+
+    // imposto un timer che fa scomparire i numeri dopo 30s
+    setTimeout(function () {
+        numbers.style.display = 'none';
+    }, seconds);
+
+}
 
 
 
@@ -43,5 +33,5 @@ startEl.addEventListener('click', function() {
 
 // funzione per generare numeri casuali
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
+    return Math.floor(Math.random() * (max - min)) + min;
 };
