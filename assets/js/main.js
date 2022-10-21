a75326d0bdd6e1b760838762750d6a7b1ce8f292
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-21 09:57:57 +0200

    add correction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,86 @@
-// dichiarazione variabili
-let seconds = 3 * 1000;
-let rndNum = [];
+//Visualizzare in pagina 5 numeri casuali.
+
+// - select a dom element where insert the numbers with querySelector
+const numbersElement = document.querySelector('.numbers')
+// - use math.floor and math.random to generate 5 random numbers
+
+const randNumbers = generateNumbers(1, 100, 5)
+console.log(randNumbers);
 
 
-// seleziono l'elemento della DOM dove mostrare i numeri
-const containerEl = document.querySelector('.container');
+function generateNumbers(min, max, max_numbers) {
+  const numbers = []
+  while (numbers.length !== max_numbers) {
+    const randomNumber = generateRandomNumber(min, max)
+    if (!numbers.includes(randomNumber)) {
+      numbers.push(randomNumber)
+    }
+  }
+  return numbers
+}
+
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+// - use innerHTML to place the numbers onto the screen
+numbersElement.innerHTML = randNumbers.toString()
 
 
-// genero 5 numeri casuali con un ciclo for
-for (let i = 0; i < 5; i++) {
-    const numGen = getRndInteger(1, 100);
-    rndNum.push(numGen);
 
-    // creo un elemento nella DOM dove salvare i numeri generati
-    let numbers = document.createElement('div');
-    numbers.classList.add('numbers');
-    numbers.innerHTML = `<div class="numbers">${numGen}</div>`;
+// Da lì parte un timer di 30 secondi.
+const timerElement = document.querySelector('.timer')
+let seconds = 3
+const intervalId = setInterval(function () {
+  timerElement.innerHTML = seconds
+  if (seconds === 0) {
+    clearInterval(intervalId)
+    // Dopo 30 secondi i numeri scompaiono 
+    numbersElement.innerHTML = ''
 
-    // inserisco l'elemento appena creato nella DOM
-    containerEl.appendChild(numbers);
+    // ask to the user to type 5 numbers
+    setTimeout(function () {
+      const userNumbers = askNumbers() // [1, 2, 4, 5, 6]
+      console.log(userNumbers);
+      // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. 
+      // check numbers 
+      const foundNumbers = checkNumbers(userNumbers, randNumbers)
+      console.log(foundNumbers);
+      alert('Hai trovato i seguenti numeri' + foundNumbers.toString() + ' Punti fatti: ' + foundNumbers.length)
+    }, 1000)
 
-    // imposto un timer che fa scomparire i numeri dopo 30s
-    setTimeout(function() {
-        numbers.classList.add('hidden')
-    }, seconds);
+
+  } else {
+    seconds--
+  }
+}, 1000)
+
+
+
+// L 'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+function askNumbers() {
+  const found = []
+  while (found.length !== 5) {
+    const userNumber = Number(prompt('type a number'))
+    console.log(userNumber);
+    if (!found.includes(userNumber)) {
+      found.push(userNumber)
+    } else {
+      alert('Hai giá inserito questo numero!')
+    }
+  }
+  return found
 }
 
 
 
-
-
-
-
-// FUNCTIONS
-
-// funzione per generare numeri casuali
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-};
-
-// funzione per mostrare il prompt
-function start() {
-    let userNumbers = [];
-    for (let i = 0; i < 5; i++) {
-        const userAnswer = Number(prompt(`Inserisci il ${i + 1}° numero mostrato`));
-        userNumbers.push(userAnswer);
+function checkNumbers(userNumbers, pcNumbers) {
+  const foundNumbers = []
+  for (let i = 0; i < userNumbers.length; i++) {
+    const number = userNumbers[i];
+    if (pcNumbers.includes(number)) {
+      foundNumbers.push(number)
     }
+  }
+  console.log(foundNumbers);
+  return foundNumbers
 }
